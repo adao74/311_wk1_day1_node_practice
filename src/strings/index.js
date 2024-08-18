c6d606595a7dd1baa7377519c15cf9b2777534fb
adao74 <andreaminghweidao@hotmail.com>
2024-08-17 23:23:20 -0500

    add string function

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,11 +1,13 @@
 const split = (str, delim) => {
   // write code for strings.split
 
+  return str.split(delim)
+
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  
 }
 
 const reverse = (str) => {
